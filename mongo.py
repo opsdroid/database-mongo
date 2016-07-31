a665aa0fb90755ba6ebb52dd7e3ee6d9e257037c
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-07-31 23:05:48 +0100

    Load database

--- mongo.py
+++ mongo.py
@@ -11,10 +11,14 @@ class DatabaseMongo:
         self.name = "mongo"
         self.config = config
         self.client = None
+        self.db = None
 
     def connect(self):
         """Connect to the database."""
         host = self.config["host"] if "host" in self.config else "localhost"
         port = self.config["port"] if "port" in self.config else "27019"
+        database = self.config["database"] \
+            if "database" in self.config else "opsdroid"
         path = "mongodb://" + host + ":" + port
         self.client = MongoClient(path)
+        self.db = self.client[database]
